fbded20c5d0f2f1b14847c1dbfbc6c063cb48fc5
Maria Osmolovskaya <maria.osmolovskaya@gmail.com>
2022-12-15 00:58:06 +0300

    Задача 29

---
+++ Desktop/GB/C# Programming/Homework4/HomeTask04-03/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] GetMassive ()
+{
+int [] massive = new int [8];
+Random rnd = new Random();
+for (int i = 0; i < massive.Length; i++)
+{
+   massive [i] = rnd.Next (0,100);
+   
+}
+return massive;
+}
+
+int [] array = GetMassive ();
+
+for (int i = 0; i < array.Length; i++)
+
+Console.Write($"{array[i]}, ");
